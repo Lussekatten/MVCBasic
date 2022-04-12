61a74ee36eaf8aac03761c2a69e846ff880b720b
Lussekatten <lucian.ralea@gmail.com>
2022-04-12 10:08:46 +0200

    Add project files.

---
+++ Controllers/MyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCEmpty.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewBag.Date = DateTime.Now.ToString();
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "Lucian testar viewbag";
+            ViewBag.Date = DateTime.Now.ToString();
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewBag.Date = DateTime.Now.ToString();
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            ViewBag.Date = DateTime.Now.ToString();
+            return View();
+        }
+    }
+
+}
